0d1efc213172d913dd1f14d4ff226eb1c2bba8eb
Ziyad <z1927za@gmail.com>
2023-09-30 18:38:12 +0100

    solution to the exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+from math import ceil, sqrt
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0: raise ValueError(f"The number of primes: {number_of_primes} is less than 1")
+    list.append(2)
+    prime = 1
+    while number_of_primes > 1:
+        prime+=1
+        for divisor in range(2, ceil(sqrt(prime))+1):
+            if prime % divisor == 0:
+                break
+            elif divisor == ceil(sqrt(prime)):
+                list.append(prime)
+                number_of_primes-=1
+    
     return list
